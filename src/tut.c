d3874e69880c951ec27a2ae3015c3fdc3d4bbf3d
ilepow34 <isaac.lepow34@gmail.com>
2015-07-19 21:36:19 -0400

    Time until 4:20 working, snoop picture added
    Time to 4:20 works but needs to be fixed for SDK 3.0. Snoop picture is there, just needs implementing.

--- src/tut.c
+++ src/tut.c
@@ -1,8 +1,12 @@
 #include <pebble.h>
+#include <stdio.h>
   
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 static TextLayer *s_420_layer;
+static Window *s_image_window;
+static GBitmap *s_bitmap;
+static BitmapLayer *s_bitmap_layer;
 
 static void update_time(){
   time_t temp = time(NULL);
@@ -16,9 +20,23 @@ static void update_time(){
 }
 
 static void update_420(){
+  int difference;
+  time_t current_time = time(NULL);
   time_t timestamp_420 = clock_to_timestamp(TODAY, 4, 20);
   time_t timestamp_1620 = clock_to_timestamp(TODAY, 16, 20);
+  int timestamp1 = timestamp_420;
+  int timestamp2 = timestamp_1620;
+  int timestamp3 = current_time;
+  if(timestamp_420 - current_time > 86400)
+    timestamp_420 -= 604800;
+  if ((timestamp_420 - current_time) < 43200)
+    difference = timestamp_420 - current_time;
+  else
+    difference = timestamp_1620 - current_time;
+  difference = difference / 60;
   static char time_to_420[] = "00 hours and\n00 minutes\nuntil 4:20";
+  //snprintf(time_to_420, sizeof("00 hours and\n00 minutes\nuntil 4:20"), "%d\n%d", timestamp1 - timestamp3, timestamp2 - timestamp3);
+  snprintf(time_to_420, sizeof("00 hours and\n00 minutes\nuntil 4:20"), "%d hours and\n%d minutes\nuntil 4:20", difference / 60, difference % 60);
   text_layer_set_text(s_420_layer, time_to_420);
 }
 
@@ -46,15 +64,32 @@ static void main_window_load(Window *window){
 
 static void main_window_unload(Window *window){
   text_layer_destroy(s_time_layer);
-  
+}
+
+static void image_window_load(Window *window){
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+  s_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SNOOP);
+  s_bitmap_layer = bitmap_layer_create(bounds);
+  bitmap_layer_set_bitmap(s_bitmap_layer, s_bitmap);
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_bitmap_layer));
+}
+
+static void image_window_unload(Window *window){
+  bitmap_layer_destroy(s_bitmap_layer);
 }
 
 static void init(){
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
   s_main_window = window_create();
+  s_image_window = window_create();
   window_set_window_handlers(s_main_window, (WindowHandlers) {
       .load = main_window_load,
       .unload = main_window_unload
+    });
+  window_set_window_handlers(s_image_window, (WindowHandlers) {
+      .load = image_window_load,
+      .unload = image_window_unload
     });
   window_stack_push(s_main_window, true);
 }
